410e2101dc3ecf27b1ccefe492b2648e326375af
Chris Bentley <chris.bentley@wireup.io>
2017-09-01 13:36:47 +0200

    WIP initial commit

---
+++ index.js
@@ -0,0 +1,144 @@
+
+var Wunderground = require('wundergroundnode');
+var myKey = '7c479cf842a78e93';
+var wunderground = new Wunderground(myKey);
+
+var SerialPort = require('serialport');
+var port = new SerialPort('COM14', { autoOpen: false });
+
+// Open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message);
+})
+
+port.open(function (err) {
+  if (err) {
+    return console.log('Error opening port: ', err.message);
+  }
+
+  console.log('Ramsberg Wetterstation Anzeigetafel')
+});
+
+// The open event is always emitted
+port.on('open', function() {
+  updateDisplay()
+})
+
+
+function updateDisplay(){
+
+wunderground.conditions().request('Germany/Pleinfeld', function(err, response){
+
+    var time = new Date(response.current_observation.local_time_rfc822)
+    var hours = time.getHours().toString()
+    var minutes= time.getMinutes().toString()
+    var wind_degrees = response.current_observation.wind_degrees
+    var wind_kph = response.current_observation.wind_kph
+    var temp_c = response.current_observation.temp_c
+    var pressure_mb = response.current_observation.pressure_mb
+
+    // correct Hours to 2 characters
+    if(hours.length < 2){
+      hours = '0'+hours
+    }
+
+    // correct minutes to 2 characters
+    if(minutes.length < 2){
+      minutes = '0'+minutes
+    }
+
+
+
+
+// input is kmh, output is beaufort number
+// initialValue is 0 so we can safely add to it to get the reduce.
+// TODO add code for (E)Fujita EF0-EF5, and Saffir-Simpson
+// EF0: 8-11
+// EF1: 12
+//  ...
+// EF5: 16
+// SafSim: Cat1/2: 12
+//         Cat3/4: 13
+//         Cat5  : 14
+//
+
+nu = function(kmh) {
+  // undefined for negative values...
+  if(kmh < 0 || kmh == undefined) return undefined;
+
+  var beauNum = kmhLimits.reduce(function(previousValue, currentValue, index, array) {
+    return previousValue + (kmh > currentValue ? 1 : 0);
+  },0);
+
+  return beauNum;
+}
+
+
+
+    // Beaufor conversion
+    // each array element is the max windspeed in kmh for that beaufort
+    // number (starting with 1)
+    // http://about.metservice.com/assets/downloads/learning/winds_poster_web.pdf
+    var kmhLimits = [1,6,11,19,30,39,50,61,74,87,102,117,177,249,332,418,512];
+
+    if(wind_kph < 0 || wind_kph == undefined) return 0;
+
+    var wind_beaufort = kmhLimits.reduce(function(previousValue, currentValue, index, array) {
+      return previousValue + (wind_kph > currentValue ? 1 : 0);
+    },0);
+
+    wind_beaufort = wind_beaufort.toString()
+
+
+    // correct wind to round value, e.g 1.1 -> 1
+    wind_kph = Math.abs(wind_kph)
+    wind_kph = Math.round(wind_kph)
+    wind_kph =   wind_kph.toString()
+
+    // correct minutes to 2 characters
+    if(wind_kph.length < 2){
+      wind_kph = '0'+wind_kph
+    }
+
+    // correct wind to temp_c value, e.g 1.1 -> 1
+    temp_c = Math.abs(temp_c)
+    temp_c = Math.round(temp_c)
+    temp_c = temp_c.toString()
+
+    // correct temp_c to 2 characters
+    if(temp_c.length < 2){
+      temp_c = '0'+temp_c
+    }
+
+    // correct wind_degrees
+    if(wind_degrees < 0){
+        wind_degrees=0
+    }
+
+    // convert wind_degrees to string
+    var val = Math.floor((wind_degrees / 22.5) + 0.5);
+    var arr = ["__N", "NNE", "_NE", "ENE", "__E", "ESE", "_SE", "SSE", "__S", "SSW", "_SW", "WSW", "__W", "WNW", "_NW", "NNW"];
+    wind_degrees = arr[(val % 16)];
+
+    console.log('hours \t\t',hours);
+    console.log('minutes \t',minutes);
+    console.log('wind_degrees \t', wind_degrees)
+    //console.log('wind_kph \t',wind_kph);
+    console.log('wind_beaufort \t',wind_beaufort);
+    console.log('temp_c \t\t',temp_c);
+    console.log('pressure_mb \t',pressure_mb);
+
+    const buf = Buffer.from('123456789abc', 'ascii');
+
+    console.log('writing ascii',buf.toString('ascii'));
+
+    port.write(buf, function(err) {
+      if (err) {
+        return console.log('Error on write: ', err.message);
+      }
+      console.log('message written');
+      port.close()
+    })
+
+  })
+}
